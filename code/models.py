36ba2fe1480f2316c0eaf900795e44fd6e81a50a
Tiago Fassoni <tiagofassoni@gmail.com>
2024-02-29 22:28:11 -0300

    A bunch of changes I'm too tired to explain

---
+++ code/models.py
@@ -0,0 +1,32 @@
+"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+    db.app = app
+    db.init_app(app)
+
+
+class Users(db.Model):
+    """Users."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    username = db.Column(db.String(50),
+                         nullable=False,
+                         unique=True)
+    first_name = db.Column(db.String(50),
+                           nullable=False,
+                           unique=True)
+    last_name = db.Column(db.String(30), nullable=True)
+    img_url = db.Column(db.String, default="images/default_user_img.png")
+
+    def __repr__(self):
+        """Show info about user."""
+        return f"<User {self.id} {self.username} {self.first_name} {self.last_name}>"
